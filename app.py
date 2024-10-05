7d7cf5ca3fef9b583ec7079315cc1ee77ff7c137
MingFei2001 <mingfei2001@gmail.com>
2024-10-05 14:44:37 +0800

    added new templates and routes

--- app.py
+++ app.py
@@ -6,4 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-app.run()
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/template')
+def template():
+    return render_template("template.html")
+
+app.run(debug=True)
